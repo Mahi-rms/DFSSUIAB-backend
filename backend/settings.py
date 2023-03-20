e5f5936d8fdf4a18d30c726592cb6bbffd8ec262
Mahi-rms <mahesh2019@iiitkottayam.ac.in>
2023-03-21 00:07:52 +0530

    Updated settings

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ DEBUG = os.environ.get('DEBUG')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST')]
 
 
 # Application definition
