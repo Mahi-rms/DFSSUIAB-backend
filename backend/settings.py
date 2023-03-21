2b4adb378aa095e60ee6b2d510531f479c84651a
Mahi-rms <mahesh2019@iiitkottayam.ac.in>
2023-03-21 10:39:19 +0530

    Added Staticfiles

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ DEBUG = os.environ.get('DEBUG')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST')]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -176,7 +176,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
 MEDIA_URL='/media/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
