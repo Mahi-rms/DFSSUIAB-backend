3b6b6b318e94d613d58483d36eee3e2c28deb782
Mahi-rms <mahesh2019@iiitkottayam.ac.in>
2023-03-23 15:37:50 +0530

    Updated Settings

--- backend/settings.py
+++ backend/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'whitenoise.runserver_nostatic',
+    'whitenoise.runserver_nostatic',
     'corsheaders',
     'drf_yasg',
     'rest_framework',
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    #"whitenoise.middleware.WhiteNoiseMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
@@ -185,10 +185,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS=[
+""" STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static'),
-]
-#STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+] """
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL='/media/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
